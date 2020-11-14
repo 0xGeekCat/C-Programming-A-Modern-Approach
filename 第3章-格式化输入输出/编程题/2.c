204a9f635ffd1ebae57c9e46b80038074d5b95d5
0xGeekCat <1292881925@qq.com>
2020-11-14 22:52:13 +0800

    《C语言程序设计现代方法(第2版)》

---
+++ 第3章-格式化输入输出/编程题/2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+//    int number;
+//    float price;
+//
+//    int month, day, year;
+//
+//    printf("Enter item number: ");
+//    scanf("%d", &number);
+//
+//    printf("Enter unit price: ");
+//    scanf("%7f", &price);
+//
+//    printf("Enter a date (mm/dd/yyyy): ");
+//    scanf("%d/%d/%d", &month, &day, &year);
+//
+//    printf("Item\tUnit\tPurchase\n");
+//    printf("\t\tPrice\tDate\n");
+//
+//    printf("%d\t\t$%-.2f\t%.2d/%.2d/%d\n", number, price, month, day, year);
+//
+//    return 0;
+//}
