b5d9c4e0da85dde6d31e2086fd43fbaa761a6837
0xGeekCat <1292881925@qq.com>
2020-11-15 19:38:00 +0800

    《C语言程序设计现代方法(第2版)》

---
+++ 第5章-选择语句/编程题/2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+//    int hour, minute;
+//
+//    printf("Enter a 24-hour time: ");
+//    scanf("%d:%d", &hour, &minute);
+//
+//    printf("Equivalent 12-hour time: %d:%d ", (hour > 12) ? hour - 12 : minute, minute);
+//
+//    if (hour > 12)
+//        printf("PM");
+//    else
+//        printf("AM");
+//
+//    return 0;
+//}
