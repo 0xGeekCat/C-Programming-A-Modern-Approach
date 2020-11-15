87ad66b64a3a9cd103277686e7a97c7944b346f9
0xGeekCat <1292881925@qq.com>
2020-11-15 14:32:30 +0800

    《C语言程序设计现代方法(第2版)》

---
+++ 第4章-表达式/编程题/1.c
@@ -0,0 +1,17 @@
+/*
+ * 如果n是整数，n % 10是个位数，n / 10是移除个位后剩下的数
+ */
+
+#include <stdio.h>
+
+//int main(void)
+//{
+//    int number;
+//
+//    printf("Enter a two-digit number: ");
+//    scanf("%d", &number);
+//
+//    printf("The reversal is: %d%d\n", number % 10, number / 10);
+//
+//    return 0;
+//}
---
+++ 第4章-表达式/编程题/3.c
@@ -0,0 +1,17 @@
+/*
+ * 本章编程题过于简单，为加快学习进度将其略过
+ */
+
+#include <stdio.h>
+
+//int main(void)
+//{
+//    int number1, number2, number3;
+//
+//    printf("Enter a three-digit number: ");
+//    scanf("%1d%1d%1d", &number1, &number2, &number3);
+//
+//    printf("The reversal is: %d%d%d\n", number3, number2, number1);
+//
+//    return 0;
+//}
