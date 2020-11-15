88c1cd9031bd2594253ae3e5731f934ce410b9e7
0xGeekCat <1292881925@qq.com>
2020-11-15 11:30:09 +0800

    《C语言程序设计现代方法(第2版)》

--- 第3章-格式化输入输出/编程题/4.c
+++ 第3章-格式化输入输出/编程题/4.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int number_1, number_2, number_3;
-
-    printf("Enter phone number [(xxx) xxx-xxxx]: ");
-    scanf("(%d)%d-%d", &number_1, &number_2, &number_3);
-
-    printf("%d.%d.%d", number_1, number_2, number_3);
-
-    return 0;
-}
+//int main(void)
+//{
+//    int number_1, number_2, number_3;
+//
+//    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+//    scanf("(%d)%d-%d", &number_1, &number_2, &number_3);
+//
+//    printf("%d.%d.%d", number_1, number_2, number_3);
+//
+//    return 0;
+//}
