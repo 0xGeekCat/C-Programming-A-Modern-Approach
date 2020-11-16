8622ad3658b1cb1d1eb9fd61bde02312da82f6fe
0xGeekCat <1292881925@qq.com>
2020-11-16 15:18:01 +0800

    《C语言程序设计现代方法(第2版)》

---
+++ 第6章-循环/程序/square.c
@@ -0,0 +1,20 @@
+/*
+ * 显示平方表
+ * 此处对源码进行了简写
+ */
+
+#include <stdio.h>
+
+//int main(void)
+//{
+//    int i = 0, n;
+//
+//    printf("This program prints a table of squares.\n");
+//    printf("Enter number of entries in tables: ");
+//    scanf("%d", &n);
+//
+//    while (i++ < n)
+//        printf("%10d%10d\n", i, i * i);
+//
+//    return 0;
+//}
---
+++ 第6章-循环/编程题/2.c
@@ -0,0 +1,23 @@
+/*
+ * Euclid算法: 求最大公约数的经典算法
+ */
+
+#include <stdio.h>
+
+//int main()
+//{
+//    int m, n;
+//
+//    printf("Enter two integers: ");
+//    scanf("%d %d", &m, &n);
+//
+//    for (int remainder; n != 0; ) {
+//        remainder = m % n;
+//        m = n;
+//        n = remainder;
+//    }
+//
+//    printf("Greatest common divisor: %d\n", m);
+//
+//    return 0;
+//}
