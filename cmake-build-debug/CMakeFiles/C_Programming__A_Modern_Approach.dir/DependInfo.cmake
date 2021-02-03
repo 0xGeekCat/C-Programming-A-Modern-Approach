6e337c74039980ed8b053f88a1d2e3d714ef9b37
0xGeekCat <1292881925@qq.com>
2021-02-03 23:08:08 +0800

    《C语言程序设计现代方法(第2版)》

---
+++ cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/tmp.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/tmp.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第10章-程序结构/程序/guess.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第10章-程序结构/程序/guess.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第10章-程序结构/程序/guess2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第10章-程序结构/程序/guess2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第10章-程序结构/程序/poker.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第10章-程序结构/程序/poker.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第11章-指针/程序/maxmin.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第11章-指针/程序/maxmin.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第12章-指针和数组/程序/reverse3.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第12章-指针和数组/程序/reverse3.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/程序/celsius.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/程序/celsius.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/程序/dweight.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/程序/dweight.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/程序/dweight2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/程序/dweight2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/程序/pun.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/程序/pun.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/编程题/2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/编程题/2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/编程题/3.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/编程题/3.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/编程题/4.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/编程题/4.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/编程题/5.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/编程题/5.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/编程题/6.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/编程题/6.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第2章-C语言基本概念/编程题/7.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第2章-C语言基本概念/编程题/7.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第3章-格式化输入输出/程序/addfrac.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第3章-格式化输入输出/程序/addfrac.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第3章-格式化输入输出/程序/tprintf.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第3章-格式化输入输出/程序/tprintf.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第3章-格式化输入输出/编程题/1.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第3章-格式化输入输出/编程题/1.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第3章-格式化输入输出/编程题/2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第3章-格式化输入输出/编程题/2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第3章-格式化输入输出/编程题/3.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第3章-格式化输入输出/编程题/3.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第3章-格式化输入输出/编程题/4.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第3章-格式化输入输出/编程题/4.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第3章-格式化输入输出/编程题/5.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第3章-格式化输入输出/编程题/5.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第3章-格式化输入输出/编程题/6.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第3章-格式化输入输出/编程题/6.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第4章-表达式/程序/upc.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第4章-表达式/程序/upc.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第4章-表达式/编程题/1.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第4章-表达式/编程题/1.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第4章-表达式/编程题/2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第4章-表达式/编程题/2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第4章-表达式/编程题/3.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第4章-表达式/编程题/3.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第5章-选择语句/程序/broker.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第5章-选择语句/程序/broker.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第5章-选择语句/程序/date.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第5章-选择语句/程序/date.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第5章-选择语句/编程题/10.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第5章-选择语句/编程题/10.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第5章-选择语句/编程题/2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第5章-选择语句/编程题/2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第5章-选择语句/编程题/4.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第5章-选择语句/编程题/4.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第5章-选择语句/编程题/6.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第5章-选择语句/编程题/6.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/程序/checking.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/程序/checking.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/程序/numdigit.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/程序/numdigit.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/程序/square.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/程序/square.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/程序/square2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/程序/square2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/程序/square3.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/程序/square3.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/程序/sum.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/程序/sum.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/编程题/2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/编程题/2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/编程题/4.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/编程题/4.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/编程题/6.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/编程题/6.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第6章-循环/编程题/8.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第6章-循环/编程题/8.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第7章-基本类型/程序/length.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第7章-基本类型/程序/length.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第7章-基本类型/程序/sum2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第7章-基本类型/程序/sum2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第8章-数组/程序/deal.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第8章-数组/程序/deal.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第8章-数组/程序/interest.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第8章-数组/程序/interest.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第8章-数组/程序/repdigit.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第8章-数组/程序/repdigit.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第8章-数组/程序/reverse.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第8章-数组/程序/reverse.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第8章-数组/程序/reverse2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第8章-数组/程序/reverse2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第9章-函数/程序/average.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第9章-函数/程序/average.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第9章-函数/程序/countdown.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第9章-函数/程序/countdown.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第9章-函数/程序/prime.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第9章-函数/程序/prime.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第9章-函数/程序/pun2.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第9章-函数/程序/pun2.c.o"
+  "/Users/max/CLionProjects/C Programming: A Modern Approach/第9章-函数/程序/qsort.c" "/Users/max/CLionProjects/C Programming: A Modern Approach/cmake-build-debug/CMakeFiles/C_Programming__A_Modern_Approach.dir/第9章-函数/程序/qsort.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
